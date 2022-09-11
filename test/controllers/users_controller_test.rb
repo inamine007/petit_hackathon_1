4592b87b107cb01c842d43c2f7f191279122fe55
hayato <ha8to.f513@gmail.com>
2022-09-12 00:21:16 +0900

    サインアップ機能の実装

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,10 +4,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "ユーザーページ" do
     get users_path
-    assert_response :success
+    assert_response :succes
   end
+
   test "サインアップpath" do
     get signup_path
-    assert_response :success
+    assert_response :succes
   end
 end
