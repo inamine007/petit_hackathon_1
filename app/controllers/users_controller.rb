291e73f35b55ae1bf11f9068335f0b28074a313f
hayato <ha8to.f513@gmail.com>
2022-09-12 00:21:16 +0900

    サインアップ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "アカウント登録成功しました。#{@user.name}さん、ようこそ！！"
+      redirect_to @user
+    else
+      flash.now[:failed] = "アカウント登録に失敗しました"
+      render 'new'
+    end
+    binding.irb
   end
 
   def show
     @user = User.find(params[:id])
   end
 end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
