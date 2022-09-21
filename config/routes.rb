dfc9172c9f2f096a1a39efac0449b38f0dea4a70
Takako <takakoyamaguchi.0204@gmail.com>
2022-09-21 10:37:20 +0900

    画像とコメント紐付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users    # usersテーブルに登録したユーザーのページ
+
+  # 画像とコメントを紐づけるルーティング
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 end
