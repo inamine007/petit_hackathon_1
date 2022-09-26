519b6c5ec12ca231434650648cfd191edd42a25f
Takako <takakoyamaguchi.0204@gmail.com>
2022-09-26 16:48:50 +0900

    整えました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
+
     helper_method :current_user
+    include SessionsHelper  # => どのコントローラーからでも、sessions_helperが呼び出せるようになる
 
     private
 
-        def current_user
-            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-        end
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end
-=======
-    include SessionsHelper  # => どのコントローラーからでも、sessions_helperが呼び出せるようになる
-end
->>>>>>> fukuchi
+
