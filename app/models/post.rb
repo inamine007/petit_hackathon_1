ee823acf78ce477090ab5055dd93cd161e587328
Takako <takakoyamaguchi.0204@gmail.com>
2022-09-29 22:25:03 +0900

    favorited_by?定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  #引数で渡されたユーザidがFavoritesテーブル内に存在するかどうかを調べるメソッド
+  def favorited_by?(user)
+    # favorites.where(user_id: user.id).exists?と同じ
+    favorites.exists?(user_id: user.id)
+  end
+  
   validates :title, presence: true
   validates :image, presence: true  
 end
